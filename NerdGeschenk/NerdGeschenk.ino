4d228fa5f99a85fc3eec358b3ff051fee29eb772
HACKER3000 <github@hacker3000.cf>
2022-12-15 10:20:03 +0100

    progress

--- NerdGeschenk/NerdGeschenk.ino
+++ NerdGeschenk/NerdGeschenk.ino
@@ -28,26 +28,18 @@ void led_wr_color_byte(/*std::initializer_list<uint32_t>&*/ uint32_t* colors) {
   for (uint8_t i = 0; i < 7; i++) {
     leds.setPixelColor(i, colors[i]);
   }
+  leds.show();
+}
+
+void led_clear() {
+  for (uint8_t i = 0; i < 7; i++) leds.setPixelColor(i, 0);
 }
 
 uint32_t seasonal_color(uint8_t& month) {
   return 0xFFFFFFFF;
 }
 
-void setup() {
-  leds.begin();
-  leds.show();
-
-  //rtc setup
-  rtc.init();
-  if (rtc.isHalted()) {
-    for (uint8_t i = 0; i < 7; i++) {
-      leds.setPixelColor(i, leds.Color((i + 1) * 36 /* 7->252 */, 0, 0));
-    }
-    while (true) delay(1000);
-  }
-  rtc.getDateTime(&dt);
-
+void led_run_daily_anim() {
   //boot animation
   switch (dt.month) {
     case Ds1302::MONTH_DEC:
@@ -57,6 +49,28 @@ void setup() {
           for (uint8_t i = 0; i < strlen(xmas); i++) {
             led_wr_byte(xmas[i], leds.Color(255, 255, 255));
             delay(1000);
+          }
+        } else if (dt.day == 31) {  //silvester
+          for (uint8_t k = 0; k <= 3; k++) {
+            for (uint8_t j = 0; j < 7; j++) {  //rakete einfliegen
+              for (uint8_t i = 0; i <= j; i++) {
+                uint8_t brght = map(i, 0, 6, 0, 255);
+                leds.setPixelColor(i, (k == 0) * brght, (k == 1) * brght, (k == 2) * brght);
+                leds.show();
+                delay(50);
+              }
+            }
+            for (uint8_t b = 1; b <= 4; b++) {
+              for (uint8_t j = 0; j < 7; j++) {  //rakete explosion
+                for (int8_t i = j; i >= 0; i--) {
+                  uint8_t brght = map(i, 0, 6, 0, b * 64);
+                  leds.setPixelColor(i, brght, brght, brght);
+                  leds.show();
+                  delay(10);
+                }
+              }
+            }
+            led_clear();
           }
         }
       }
@@ -71,7 +85,7 @@ void setup() {
                                    leds.Color(0, 192, 255),
                                    leds.Color(0, 0, 255),
                                    leds.Color(192, 0, 255) };
-        led_wr_color_byte(pride_flag);  //keine STL, keine einfache moeglichkeit das direkt als initializer list zu passen
+        led_wr_color_byte(pride_flag);  //keine ordentliche STL, keine einfache moeglichkeit das direkt als initializer list zu passen
         leds.show();
         delay(3000);
       }
@@ -92,13 +106,31 @@ void setup() {
         for (uint8_t i = 0; i <= 4; i++) {
           leds.setPixelColor(i, seasonal_color(dt.month));
           leds.setPixelColor(6 - i, seasonal_color(dt.month));
+          leds.show();
           delay(100);
         }
         break;
       }
   }
-
   led_wr_byte(0, 0);
+}
+
+void setup() {
+  leds.begin();
+  leds.show();
+
+  //rtc setup
+  rtc.init();
+  if (rtc.isHalted()) {
+    for (uint8_t i = 0; i < 7; i++) {
+      leds.setPixelColor(i, (i + 1) * 36 /* 7->252 */, 0, 0);
+    }
+    leds.show();
+    while (true) delay(1000);
+  }
+  rtc.getDateTime(&dt);
+
+  led_run_daily_anim();
 }
 
 enum time_page_e {
@@ -115,7 +147,7 @@ bool edit_mode = false;
 auto last_disp_change = millis();
 void display_page() {
   if (millis() > last_disp_change - 5000) {
-    time_page++;
+    if (!edit_mode) time_page++;
     if (time_page > PAGE_SEC) time_page = PAGE_YEAR;
   }
 
@@ -156,15 +188,54 @@ void poll_buttons() {
   uint8_t button_val = read_button_val();
 
   if ((button_val != last_button_val) and (millis() - last_button_millis > DEBOUNCE_DELAY)) {
-    if (button_val != 0) last_button_millis = millis();  //loslassen ignorieren
+    if (button_val != 0) last_button_millis = millis();
 
-    switch (button_val) {
+    //TODO: button reagiert auf loslassen, lange halten setzt feld zurueck
+
+    switch (last_button_val) {
       case 1:
+        if (edit_mode) {
+          switch (time_page) {
+            case PAGE_YEAR:
+              dt.year++;
+              break;
+            case PAGE_MON:
+              dt.month++;
+              if (dt.month > 12) dt.month = 0;
+              break;
+            case PAGE_DAY:
+              if (dt.month > 31) dt.month = 0;
+              break;
+            case PAGE_HOUR:
+              if (dt.month > 24) dt.month = 0;
+              break;
+            case PAGE_MIN:
+              if (dt.month > 60) dt.month = 0;
+              break;
+            case PAGE_SEC:
+              if (dt.month > 60) dt.month = 0;
+              break;
+          }
+          rtc.setDateTime(&dt);
+
+        } else {
+          uint8_t led_bright = leds.getBrightness();
+          led_bright += 32;
+          leds.setBrightness(led_bright);
+        }
         break;
 
       case 2:
+        if (!edit_mode) {
+          edit_mode = true;
+          time_page = 0;
+        } else {
+          time_page++;
+        }
+
         break;
     }
+    last_button_val = button_val;
   }
 }
 
